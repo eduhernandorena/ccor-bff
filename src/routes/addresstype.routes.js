42b22b16fce3d6089679f661680dbcd6ab90452b
Eduardo Hernandorena <luiz.barbosa@gavb.com.br>
2019-07-25 13:02:37 -0300

    alterada a ordem das rotas.

--- src/routes/addresstype.routes.js
+++ src/routes/addresstype.routes.js
@@ -2,10 +2,10 @@ const addressTypeControler = require('../controllers/addresstype.controller')
 let router = require('express').Router()
 
 
-router.get('/addresstype/:id', addressTypeControler.getAddressTypeById)
-router.put('/addresstype', addressTypeControler.putAddressType)
-router.delete('/addresstype/:id', addressTypeControler.deleteAddressType)
 router.get('/addresstype', addressTypeControler.getAllAddressType)
+router.get('/addresstype/:id', addressTypeControler.getAddressTypeById)
+router.delete('/addresstype/:id', addressTypeControler.deleteAddressType)
+router.put('/addresstype', addressTypeControler.putAddressType)
 router.post('/addresstype', addressTypeControler.postAddressType)
 
 module.exports = router
